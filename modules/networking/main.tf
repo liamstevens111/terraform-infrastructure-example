15f871ec83371860b4e0112f2952b1780aa05906
Liam Stevens <8955671+liamstevens111@users.noreply.github.com>
2022-12-13 16:09:52 +0700

    Add placeholder ALB and security groups in prepariation for ECS

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -45,3 +45,50 @@ resource "aws_subnet" "private_2" {
     Name = "Private Subnet 1"
   }
 }
+
+resource "aws_security_group" "alb_ecs_security_group" {
+  name        = "alb_ecs_security_group"
+  description = "Security group for ECS ALB"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "alb_ecs_security_group"
+  }
+}
+
+resource "aws_security_group_rule" "ecs_allow_incoming_http" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.alb_ecs_security_group.id
+}
+
+resource "aws_security_group_rule" "ecs_allow_incoming_https" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.alb_ecs_security_group.id
+}
+
+resource "aws_security_group_rule" "outgoing_to_ecs" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb_ecs_security_group.id
+}
+
+resource "aws_lb" "alb_ecs_main" {
+  name               = "alb-ecs-main"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_ecs_security_group.id]
+  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+}
