5f60c547b923f2b5c0ba1f2e73934be015012f38
Liam Stevens <8955671+liamstevens111@users.noreply.github.com>
2022-12-14 08:08:10 +0700

    Reformat and refactor

---
+++ modules/network/variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_name" {
+  type = string
+}
+
+variable "base_cidr_block" {
+  type = string
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,8 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+variable "env_name" {
+  type        = string
+  default     = "prod"
+  description = "Name to be used for the current environment, ie staging or production"
+}
