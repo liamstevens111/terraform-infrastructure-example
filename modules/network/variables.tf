6c04a77d54426d9e9c9e417de33a102185551f8e
Liam Stevens <8955671+liamstevens111@users.noreply.github.com>
2022-12-20 08:21:00 +0700

    Refactor variable and remove vpc name to use env name. Fix ingress from alb

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,13 +1,8 @@
-variable "vpc_name" {
-  type = string
-}
-
 variable "base_cidr_block" {
   type = string
 }
 
 variable "env_name" {
   type        = string
-  default     = "prod"
   description = "Name to be used for the current environment, ie staging or production"
 }
